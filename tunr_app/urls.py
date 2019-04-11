ab7a44c08208e665672061eacdcb70567e2ad712
Mandy <mandysommerfield@gmail.com>
2019-04-11 12:26:41 -0400

    added method in serializer

--- tunr_app/urls.py
+++ tunr_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework import routers
 from . import views
 
-router = routers.DefaultRouter
+router = routers.DefaultRouter()
 router.register('artists', views.ArtistView)
 router.register('songs', views.SongView)
 
